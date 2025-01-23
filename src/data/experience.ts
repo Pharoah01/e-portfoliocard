ccf307a5b289081b5697bf9970784447994edfad
Pharoah01 <elavarasanthandapani@gmail.com>
2025-01-23 13:25:21 +0530

    Fixing push issues

---
+++ src/data/experience.ts
@@ -0,0 +1,29 @@
+import type { Props as ExperienceProp } from "../components/experience/props";
+
+export const EXPERIENCE: ExperienceProp[] = [
+  {
+    dates: "August 2023 - Present",
+    title: "Under Graduate - Degree",
+    company: "Sathyabama Institute Of Science And Technology",
+    companyUrl: "https://www.sathyabama.ac.in",
+    description:
+      "",
+    technologies: [
+      "B.E Computer Science And Engineering",
+      "Cyber Security",
+    ],
+    logo: "/logos/sathyabama.jpg",
+  },
+  {
+    dates: "June 2022 — May 2023",
+    title: "High School",
+    company: "Amrita Vidyalayam Higher Secondary",
+    companyUrl: "https://nagapattinam.amritavidyalayam.org/",
+    description:
+      "",
+    technologies: [
+      "Grade XII",
+    ],
+    logo: "/logos/amrita-vidyalayam.svg",
+  },
+];
---
+++ src/data/socials.ts
@@ -0,0 +1,21 @@
+import IconGithub from "../components/icons/github.astro";
+import IconLinkedin from "../components/icons/linkedin.astro";
+import IconTwitter from "../components/icons/twitter.astro";
+
+export const SOCIALS = [
+  {
+    name: "Github",
+    url: "https://github.com/pharoah01",
+    icon: IconGithub,
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/elavarasan-t-a5971b2a5",
+    icon: IconLinkedin,
+  },
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/_simply.jaswanth/?locale=en_GB%2Cen_US%2Cen_GB%2Cen_GB",
+    icon: IconTwitter,
+  },
+];
